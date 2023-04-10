6a1c98767a14c578b500a57b546c7bbffbcd92fb
aleynameydan <aleynameydan2703@gmail.com>
2023-04-10 18:51:36 +0300

    Initial Commit

---
+++ FinalProjectVs1/FinalProjectVs1App.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectVs1App.swift
+//  FinalProjectVs1
+//
+//  Created by Aleyna Dila Meydan on 10.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectVs1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
